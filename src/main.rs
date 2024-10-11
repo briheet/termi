55bcd7a855c28610fe6ce0de7948cb68703c84a0
briheet <briheetyadav@gmail.com>
2024-10-12 00:39:27 +0530

    fd

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,57 @@
 use eframe::egui;
+use nix::pty::forkpty;
+use nix::unistd::ForkResult;
+use std::{
+    ffi::{CStr, CString},
+    fs::File,
+    io::Read,
+    os::fd::{IntoRawFd, OwnedFd, RawFd},
+};
 
 fn main() {
+    let fd = unsafe {
+        let res = forkpty(None, None).unwrap();
+        match res.fork_result {
+            ForkResult::Parent { .. } => (),
+            ForkResult::Child => {
+                let shell_name_bytes = b"ash\0";
+                let shell_name = CStr::from_bytes_with_nul(shell_name_bytes)
+                    .expect("CStr::from_bytes_with_nul failed");
+                nix::unistd::execvp::<CString>(shell_name, &[]).unwrap();
+                return;
+            }
+        }
+        res.master
+    };
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "My egui App",
+        "Termi",
         native_options,
-        Box::new(|cc| Ok(Box::new(TermiGui::new(cc)))),
+        Box::new(move |cc| Ok(Box::new(TermiGui::new(cc, fd)))),
     );
 }
 
-#[derive(Default)]
-struct TermiGui {}
+struct TermiGui {
+    buf: String,
+    fd: File,
+}
 
 impl TermiGui {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+    fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        TermiGui {
+            buf: String::new(),
+            fd: fd.into(),
+        }
     }
 }
 
 impl eframe::App for TermiGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut buf = vec![0u8; 4098];
+        match self.fd.read(&mut buf) {}
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+            ui.heading("Welcome to Termi");
         });
     }
 }
